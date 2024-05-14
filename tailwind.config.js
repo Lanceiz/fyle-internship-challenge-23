d58f9cf1fcf8b7a44eb3793d777769ace053407d
Lanceiz <atuls.90601@gmail.com>
2024-05-14 13:17:38 +0530

    Final changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#428DCD', 
+      }
+    },
   },
   plugins: [],
 }
